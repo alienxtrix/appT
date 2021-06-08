9f36c136ae248da0f427bd035e19c20c353ae7c5
DESKTOP-UPL49TM\tensi <tensinkusum@hotmail.com>
2021-06-08 09:26:16 -0500

    archivos concatenados

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+	var resultado;
 function leerArchivo(e) {
     var archivo = e.target.files[0];
     if (!archivo) {
@@ -6,22 +7,23 @@ function leerArchivo(e) {
     var lector = new FileReader();
     lector.onload = function(e) {
       var contenido = e.target.result;
-      mostrarContenido(contenido);
+	  if( resultado == null) {resultado = contenido;}
+	  else {resultado = resultado + contenido;}
+      mostrarContenido(resultado);
     };
-    lector.readAsText(archivo);
-  }
+    lector.readAsText(archivo); 
+}
   
-  function mostrarContenido(contenido) {
+function mostrarContenido(resultado) {
     var elemento = document.getElementById('contenido-archivo');
-    elemento.innerHTML = contenido;
-  }
+    elemento.innerHTML = resultado;
+}
   
-  document.getElementById('file-input')
-    .addEventListener('change', leerArchivo, false);
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
 	
-	function savefile(){
-		var content = document.getElementById('contenido-archivo').value;
-		uriContent = "data:application/octet-stream," + encodeURIComponent(content);
-		document.getElementById("dlink").innerHTML = "<a href=" + uriContent + " download=\"NuevoArchivo.txt\">Click acá para guardar el archivo</a>";
-		alert("Haga clic en el link azul para guaradr el archivo");
-	}
+function savefile(){
+	var content = document.getElementById('contenido-archivo').value;
+	uriContent = "data:application/octet-stream," + encodeURIComponent(content);
+	document.getElementById("dlink").innerHTML = "<a href=" + uriContent + " download=\"NuevoArchivo.txt\">Click acá para guardar el archivo</a>";
+	alert("Haga clic en el link azul para guaradr el archivo");
+}
