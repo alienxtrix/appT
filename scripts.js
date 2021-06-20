76f5bda1b9fd101fa0c5c0ac9e5b3b5042cff817
DESKTOP-UPL49TM\tensi <tensinkusum@hotmail.com>
2021-06-20 18:01:53 -0500

    función que separa el mensaje

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@
 	var tama;//var que guarda el tamaño de guardados para evitar ciclos infinitos
 	var fileName;// nombre del archivo a leer
 	
-	//lovalStore.clear() // quitar lo guardado en local storage
+	localStorage.clear() // quitar lo guardado en local storage
 	
 function leerArchivo(e) {
     var archivo = e.target.files[0];
@@ -17,8 +17,8 @@ function leerArchivo(e) {
       return;
     }
 	fileName = e.target.files[0].name; // leer nombre del archivo a guardar
-	console.log("el nombre del archivo es: ");
-	console.log(fileName);
+	//console.log("el nombre del archivo es: ");
+	//console.log(fileName);
 	tama = guardados.length;//como el for incrementa guardados.length,lo ponemos en tama
 	for (var i=0; i<=tama; i++){//mirar si el nombre del archivo está guardado en el array de guardados
 		if(guardados[i] == fileName){ return; }
@@ -34,6 +34,8 @@ function leerArchivo(e) {
 	  if( resultado == null) {resultado = contenido;}
 	  else {resultado = resultado + contenido;}
       mostrarContenido(resultado);
+	  //console.log(typeof resultado);
+      clasificator();
     };
     lector.readAsText(archivo); 
 }
@@ -55,5 +57,80 @@ function savefile(){
 	a.dispatchEvent(new MouseEvent("click"));
 }
 
+function clasificator(){
 
+		//separar en variables
+		let arr = []
+		let fecha = []//arr para guardar la fecha
+		let hora = []//arr para guardar la hora
+		let tipo = []
+		let pago = []
+		let sucursal = []
+		let tarjeta = []
+		let cuenta = []
+		let importe = []
+		let estado = []
+		let referencia = []
+		arr = resultado.split("");//partir el resultado en un array
+		for(let i=0; i<8; i++){
+			fecha.push(arr[i])
+		}
+		console.log('La fecha es ')
+		console.log(fecha)	
+		//
+		for(let i=8; i<12; i++){
+			hora.push(arr[i])
+		}
+		console.log('La hora es ')
+		console.log(hora)	
+		//
+		for(let i=12; i<13; i++){
+			tipo.push(arr[i])
+		}
+		console.log('El tipo es ')
+		console.log(tipo)
+		//	
+		for(let i=13; i<14; i++){
+			pago.push(arr[i])
+		}
+		console.log('El pago es ')
+		console.log(pago)
+		//
+		for(let i=14; i<18; i++){
+			sucursal.push(arr[i])
+		}
+		console.log('La sucursal es ')
+		console.log(sucursal)	
+		//
+		for(let i=18; i<21; i++){
+			tarjeta.push(arr[i])
+		}
+		console.log('La tarjeta es ')
+		console.log(tarjeta)
+		//
+		for(let i=21; i<33; i++){
+			cuenta.push(arr[i])
+		}
+		console.log('La cuenta es ')
+		console.log(cuenta)
+		//
+		for(let i=33; i<45; i++){
+			importe.push(arr[i])
+		}
+		console.log('El importe es ')
+		console.log(importe)
+		//
+		for(let i=45; i<47; i++){
+			estado.push(arr[i])
+		}
+		console.log('El estado es ')
+		console.log(estado)	
+		//
+		for(let i=77; i<90; i++){
+			referencia.push(arr[i])
+		}
+		console.log('La referencia es ')
+		console.log(referencia)				
+
+}
 
